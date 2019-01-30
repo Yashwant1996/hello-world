aae15ef4aeba4e94df45beba07885ddb5545dfb8
Yashwant1996 <ys621290@dal.ca>
2019-01-30 15:24:28 -0400

    lab5-1

---
+++ lab5t1.py
@@ -0,0 +1,28 @@
+import math
+
+class Point:
+   
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def distance(self, point2):
+        xdiff = point2.getX()-self.getX()
+        ydiff = point2.getY()-self.getY()
+
+        dist = math.sqrt(xdiff**2 + ydiff**2)
+        return dist
+
+p = Point(4,3)
+q = Point(2,3)
+print(p.distance(q))
